bc29f49ea08f2646fe9cb85740a26b9c69746024
Daesung Moon <moondae@gmail.com>
2026-08-01 09:07:00 +0900

    feat: add DBVC setup overlay UI

---
+++ src/DBVC.Vsix/UI/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DBVC.Vsix.UI
+{
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && b) return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
